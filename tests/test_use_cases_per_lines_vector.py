b1dd647d029df005e7993e0ea144f683a6db816a
SirGnip <SirGnip@users.noreply.github.com>
2021-11-10 07:17:28 -0600

    Split tests on vector based commands into separate files

---
+++ tests/test_use_cases_per_lines_vector.py
@@ -0,0 +1,81 @@
+'''
+"Vector, per-lines" commands
+The vector of lines is mutated at the "line" level (adding or removing lines)
+as opposed to the individual string level (changing characters in each line)
+
+Index operations refer to lines in the text block
+'''
+import subprocess
+
+
+short = '''
+abcdef
+ghijkl'''.lstrip('\n')
+
+d1 = '''
+abcdef
+ghijkl
+mnop
+qrst'''.lstrip('\n')
+
+
+def do(input_text, cmd):
+    return subprocess.run(r'..\tools\src\\' + cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, input=input_text, check=False, text=True).stdout
+
+
+
+def test_slice_lines_in_text():
+    t = do(d1, 'lines_exp.py "[lines[-2]]')
+    assert t == 'mnop\n'
+
+    t = do(d1, 'lines_exp.py "lines[1:3]')
+    assert t == '''
+ghijkl
+mnop
+'''.lstrip('\n')
+
+def test_del_line():
+    # del 1
+    t = do(d1, 'lines_exp.py "lines[:1] + lines[2:]')
+    assert t == '''
+abcdef
+mnop
+qrst
+'''.lstrip('\n')
+
+    # del 1:3
+    t = do(d1, 'lines_exp.py "[lines[0],] + lines[3:]')
+    assert t == '''
+abcdef
+qrst
+'''.lstrip('\n')
+
+def test_replace():
+    # replace indexed/sliced line(s) with given line)
+    # could be accomplished by composing a delete then an insert
+    # replace 2 NEW
+    t = do(d1, '''lines_exp.py "lines[:2] + ['NEW',] + lines[3:]"''')
+    assert t == '''
+abcdef
+ghijkl
+NEW
+qrst
+'''.lstrip('\n')
+
+    # may be redundant with "delete & insert"
+    # replace 1:3 "NEW LINE"
+    t = do(d1, '''lines_exp.py "lines[:1] + ['NEW LINE',] + lines[3:]"''')
+    assert t == '''
+abcdef
+NEW LINE
+qrst
+'''.lstrip('\n')
+
+def test_insert():
+    # insert 1 NEW
+    t = do(short, '''lines_exp.py "lines[:1] + ['NEW',] + lines[1:]"''')
+    assert t == '''
+abcdef
+NEW
+ghijkl
+'''.lstrip('\n')
--- tests/test_use_cases_per_string_vector.py
+++ tests/test_use_cases_per_string_vector.py
@@ -1,4 +1,8 @@
 """
+"Vector, per-string" commands
+The vector of lines is mutated at the individual string level (changing the contents of a line)
+as opposed to the line level (adding or removing lines)
+
 
 # some kind of template formatting where lines get read, parsed into pieces and fed into a template? fstring? variable expansion? regex transform?
 
@@ -46,22 +50,7 @@ def do(input_text, cmd):
     return subprocess.run(r'..\tools\src\\' + cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, input=input_text, check=False, text=True).stdout
 
 
-def test_initial():
-    t = do(d1, 'line_exp.py "t[1]"')
-    assert t == 'b\nh\nn\nr\n'
-
 ########## Operations on vector (text block treated as a vector  of items, where each line is an item in the vector)
-# Index operations refer to lines in the text block
-def test_slice_lines_in_text():
-    t = do(d1, 'lines_exp.py "[lines[-2]]')
-    assert t == 'mnop\n'
-
-    t = do(d1, 'lines_exp.py "lines[1:3]')
-    assert t == '''
-ghijkl
-mnop
-'''.lstrip('\n')
-
 def test_slice_chars_in_each_line():
     t = do(short, 'line_exp.py "t[2]"')
     assert t == 'c\ni\n'
@@ -102,22 +91,6 @@ def test_strip():
     t = do(orig, 'line_exp.py "t.rstrip()"')
     assert t == '  abc  def\n  ghi  jkl\n'
 
-def test_del_line():
-    # del 1
-    t = do(d1, 'lines_exp.py "lines[:1] + lines[2:]')
-    assert t == '''
-abcdef
-mnop
-qrst
-'''.lstrip('\n')
-
-    # del 1:3
-    t = do(d1, 'lines_exp.py "[lines[0],] + lines[3:]')
-    assert t == '''
-abcdef
-qrst
-'''.lstrip('\n')
-
 def test_del_char_per_line():
     # del 1
     t = do(d1, 'line_exp.py "t[:1] + t[2:]')
@@ -126,36 +99,6 @@ acdef
 gijkl
 mop
 qst
-'''.lstrip('\n')
-
-def test_replace():
-    # replace indexed/sliced line(s) with given line)
-    # could be accomplished by composing a delete then an insert
-    # replace 2 NEW
-    t = do(d1, '''lines_exp.py "lines[:2] + ['NEW',] + lines[3:]"''')
-    assert t == '''
-abcdef
-ghijkl
-NEW
-qrst
-'''.lstrip('\n')
-
-    # may be redundant with "delete & insert"
-    # replace 1:3 "NEW LINE"
-    t = do(d1, '''lines_exp.py "lines[:1] + ['NEW LINE',] + lines[3:]"''')
-    assert t == '''
-abcdef
-NEW LINE
-qrst
-'''.lstrip('\n')
-
-def test_insert():
-    # insert 1 NEW
-    t = do(short, '''lines_exp.py "lines[:1] + ['NEW',] + lines[1:]"''')
-    assert t == '''
-abcdef
-NEW
-ghijkl
 '''.lstrip('\n')
 
 def test_join():
